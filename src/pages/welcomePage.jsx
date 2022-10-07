9a0bf930c3a265d811adfa4bead009a1bc7cfe0e
Robson <robson.miranda@lunes.io>
2022-10-07 10:01:20 -0300

    feature: added network verification

--- src/pages/welcomePage.jsx
+++ src/pages/welcomePage.jsx
@@ -1,7 +1,24 @@
+import { ChainId } from "@thirdweb-dev/sdk"
+
 import useDaoTokens from "../hooks/useDaoTokens"
+import { useNetwork } from "@thirdweb-dev/react"
 
 const Welcome = () => {
+    const network = useNetwork()
     const { address, connectWithMetamask } = useDaoTokens()
+
+    if (address && network?.[0].data.chain.id !== ChainId.Mumbai) {
+        return (
+            <div className="unsupported-network">
+                <h2>Por favor, conecte-se à rede Mumbai</h2>
+                <p>
+                    Essa dapp só funciona com a rede Mumbai, por favor troque de
+                    rede na sua carteira.
+                </p>
+            </div>
+        )
+    }
+
     return (
         <div className="landing">
             <h1>Bem-vindos à ExtractoDAO</h1>
